05e780060379371bcc4567ab9d683ddfd434e50a
Akashnani-star <dhanviakash1234@gmail.com>
2022-02-09 13:22:21 +0000

    Updates

---
+++ precache-manifest.274c8b96996c68dafdcead6907f52be9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b3604bcb8c9250aad9bae33f89540d",
+    "url": "/nameIt/index.html"
+  },
+  {
+    "revision": "0ac5b782f2cd963acafe",
+    "url": "/nameIt/static/css/main.c32f9fea.chunk.css"
+  },
+  {
+    "revision": "af44548e9b121c642750",
+    "url": "/nameIt/static/js/2.93c251af.chunk.js"
+  },
+  {
+    "revision": "0ac5b782f2cd963acafe",
+    "url": "/nameIt/static/js/main.c864834a.chunk.js"
+  },
+  {
+    "revision": "f7efec226af603a1d902",
+    "url": "/nameIt/static/js/runtime~main.a735cef5.js"
+  }
+]);
